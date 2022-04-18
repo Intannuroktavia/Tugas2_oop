8e4351b0867b4aeb53a1ffdd8dbe0b92e305c318
Intannuroktavia <101161878+Intannuroktavia@users.noreply.github.com>
2022-04-18 19:28:21 +0800

    Add files via upload
    File 2

---
+++ VolumeB1.java
@@ -0,0 +1,19 @@
+package VolumeB1;
+
+/**
+ *
+ * @author Intan Nur Oktavia_20103321
+ */
+public class VolumeB1 {
+
+public static void main(String[] args) {
+        
+        HitungVolumeB1 balok = new HitungVolumeB1(0,0 ,0 );
+        balok.setLebar(15);
+        balok.setPanjang(25);
+        balok.setTinggi(30);
+        balok.tapvol();
+    
+    }
+    
+}
