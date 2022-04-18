8f9e5fd67442c74689462076943a2a64a6731d36
Intannuroktavia <101161878+Intannuroktavia@users.noreply.github.com>
2022-04-18 19:26:23 +0800

    File 1

---
+++ HitungVolumeBalok.java
@@ -0,0 +1,56 @@
+package VolumeBalok;
+
+/**
+ *
+ * @author Intan Nur Oktavia_20103321
+ */
+public class HitungVolumeBalok {
+    
+    protected double pj, lb, tg, vol;
+    
+    protected HitungVolumeBalok (double Pj, double Lb, double Tg){
+        
+        this.setPanjang(Pj);
+        this.setLebar(Lb);
+        this.setTinggi(Tg);
+        
+    }
+    
+   protected void setPanjang(double Pj){
+        
+        this.pj=Pj;
+        
+    }
+    
+    protected void setLebar(double Lb){
+        
+        this.lb=Lb;
+        
+    }
+    
+    protected void setTinggi (double Tg){
+        
+        this.tg=Tg;
+        
+    }
+    
+    protected double HV(){
+        
+        this.vol= this.pj * this.lb * this.tg; 
+        return this.vol;
+        
+    }
+    
+    protected void tapvol(){
+        
+        this.HV();
+        System.out.println("Hitung Volume Balok");
+        System.out.println("======================");
+        System.out.println("Nilai Panjang = "+this.pj+"cm²");
+        System.out.println("Nilai Lebar = "+this.lb+"cm²");
+        System.out.println("Nilai Tinggi = "+this.tg+"cm²");
+        System.out.println("Nilai Volume Balok = "+this.vol+"cm³");
+        
+    }
+    
+}
---
+++ VolumeBalok.java
@@ -0,0 +1,19 @@
+package VolumeBalok;
+
+/**
+ *
+ * @author Intan Nur Oktavia_20103321
+ */
+public class VolumeBalok {
+
+    public static void main(String[] args) {
+        
+        HitungVolumeBalok balok = new HitungVolumeBalok(0,0 ,0 );
+        balok.setLebar(20);
+        balok.setPanjang(30);
+        balok.setTinggi(10);
+        balok.tapvol();
+        
+    }
+    
+}
