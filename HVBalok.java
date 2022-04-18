b5d045b4bff0df4f852e77cac11de12906c32279
Intannuroktavia <101161878+Intannuroktavia@users.noreply.github.com>
2022-04-18 19:32:41 +0800

    File 3

---
+++ HVBalok.java
@@ -0,0 +1,20 @@
+package hvbalok;
+
+/**
+ *
+ * @author Intan Nur Oktavia_20103321
+ */
+public class HVBalok {
+
+    
+    public static void main(String[] args) {
+        
+        VBalok balok = new VBalok(0,0 ,0 );
+        balok.setLebar(25);
+        balok.setPanjang(30);
+        balok.setTinggi(10);
+        balok.tapvol();
+        
+    }
+    
+}
